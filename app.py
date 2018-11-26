a28c2d59fb8603fb046e91aecb10ec7bab99626c
elisamalzoni <elisamm@al.insper.edu.br>
2018-11-26 02:04:57 -0200

    aps1

---
+++ app.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+from flask import Flask, request, jsonify
+from tarefa import Tarefa
+
+app = Flask(__name__)
+
+id_count = 0
+dic_tarefas = {} # id: objeto tarefa
+
+dic_tarefas[id_count] = Tarefa('tituloooo', 'desa dmasd', False)
+id_count += 1
+dic_tarefas[id_count] = Tarefa('aloos', 'alod', True)
+
+id_count += 1
+dic_tarefas[id_count] = Tarefa('descomp', 'estudar pra prova', False)
+
+
+@app.route('/Tarefa', methods=['GET', 'POST'])
+def taref():
+    global id_count, dic_tarefas
+    # print('dadoss', request.data)
+    if request.method == 'GET':
+        tarefas = []
+        for k, v in dic_tarefas.items():
+            t = "titulo: " + v.get_title()
+            d = "descricao: " + v.get_description()
+            s = "feita: " + str(v.get_done())
+            tarefas.append([t,d,s])
+        return jsonify(tarefas)
+    else:
+        t = request.form['title']
+        d = request.form['description']
+        s = request.form['done']
+        tar = Tarefa(t, d, s)
+        dic_tarefas[id_count+1] = tar
+        id_count += 1
+
+        tarefas = []
+        for k, v in dic_tarefas.items():
+            t = "titulo: " + v.get_title()
+            d = "descricao: " + v.get_description()
+            s = "feita: " + str(v.get_done())
+            tarefas.append([t,d,s])
+        return jsonify(tarefas), 201
+
+@app.route('/Tarefa/<int:tarefa_id>', methods=['GET', 'PUT', 'DELETE'])
+def taref_id(tarefa_id):
+    # global dic_tarefas
+
+    try:
+        if request.method == 'GET':
+            ta = dic_tarefas[tarefa_id]
+            t = "titulo: " + ta.get_title()
+            d = "descricao: " + ta.get_description()
+            s = "feita: " + str(ta.get_done())
+            return jsonify([t,d,s])
+
+        elif request.method == 'PUT':
+            ta = dic_tarefas[tarefa_id]
+            ta.set_title(request.form['title'])
+            ta.set_description(request.form['description'])
+            ta.set_done(request.form['done'])
+            t = "titulo: " + ta.get_title()
+            d = "descricao: " + ta.get_description()
+            s = "feita: " + str(ta.get_done())
+            return jsonify([t,d,s]), 200
+        
+        elif request.method == 'DELETE':
+            del dic_tarefas[tarefa_id]
+            return '200'
+    except:
+        return '404'
+
+@app.route('/healthcheck')
+def hc():
+    return '200'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tarefa.py
@@ -0,0 +1,24 @@
+class Tarefa():
+    
+    def __init__(self, title, description, done):
+        self.title = title
+        self.description = description
+        self.done = done
+    
+    def set_title(self, title):
+        self.title = title
+    
+    def set_description(self, description):
+        self.description = description
+    
+    def set_done(self, done):
+        self.done = done
+    
+    def get_title(self):
+        return self.title
+    
+    def get_description(self):
+        return self.description
+    
+    def get_done(self):
+        return self.done
