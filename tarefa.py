7a5fafacd6175b85904f7985c9e3d67f7d405a3a
elisamalzoni <elisamm@al.insper.edu.br>
2018-11-26 06:59:08 -0200

    aps2 - todos os endpoints

--- tarefa.py
+++ tarefa.py
@@ -1,24 +1,46 @@
-class Tarefa():
-    
-    def __init__(self, title, description, done):
-        self.title = title
-        self.description = description
-        self.done = done
-    
-    def set_title(self, title):
-        self.title = title
-    
-    def set_description(self, description):
-        self.description = description
-    
-    def set_done(self, done):
-        self.done = done
-    
-    def get_title(self):
-        return self.title
-    
-    def get_description(self):
-        return self.description
-    
-    def get_done(self):
-        return self.done
+#!/usr/bin/python3
+
+import sys
+import requests
+import json
+import os
+
+print(os.environ['END'])
+
+end = 'http://localhost:5000'
+
+if sys.argv[1] == 'adicionar':
+    t = sys.argv[2]
+    d = sys.argv[3]
+    s = sys.argv[4]
+    h = {'content-type': 'application/json'}
+    dp = {'title': t, 'description': d, 'done': s}
+    r = requests.post(end + '/Tarefa', data=json.dumps(dp), headers=h)
+    print(r.text)
+
+elif sys.argv[1] == 'listar':
+    r = requests.get(end + '/Tarefa')
+    print(r.text)
+
+elif sys.argv[1] == 'buscar':
+    t_id = sys.argv[2]
+    r = requests.get(end + '/Tarefa/' + t_id)
+    print(r.text)
+
+
+
+elif sys.argv[1] == 'apagar':
+    t_id = sys.argv[2]
+    r = requests.delete(end + '/Tarefa/' + t_id)
+    print(r.text)
+
+elif sys.argv[1] == 'atualizar':
+    t_id = sys.argv[2]
+    t = sys.argv[3]
+    d = sys.argv[4]
+    s = sys.argv[5]
+    h = {'content-type': 'application/json'}
+    dp = {'title': t, 'description': d, 'done': s}
+    r = requests.put(end + '/Tarefa/'+ t_id, data=json.dumps(dp), headers=h)
+    print(r.text)
+
